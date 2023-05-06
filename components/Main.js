933b8219cb29430f60f38282725107c35167fa30
willy161 <gasper.jezovsek@gmail.com>
2023-05-07 00:42:38 +0200

    error saying indexOf...?

--- components/Main.js
+++ components/Main.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {app} from './auth/firebaseConfig';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchUser, fetchUserPosts } from '../redux/actions/index';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FeedScreen from './main/Feed';
+import SearchScreen from './main/Search';
 import ProfileScreen from './main/Profile';
 
 const EmptyScreen = () => {
@@ -37,7 +38,20 @@ export class Main extends Component {
             <MaterialCommunityIcons name="home" color={color} size={26} />
           )
         }}/>
-        <Tab.Screen name="Profile" component={ProfileScreen} 
+        <Tab.Screen name="Search" component={SearchScreen} navigation = {this.props.navigation} 
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="magnify" color={color} size={26} />
+          )
+        }}/>
+        <Tab.Screen name="Profile" component={ProfileScreen}
+         listeners={({ navigation }) => ({
+          tabPress: event => {
+              console.log("Profile button pressed");
+              event.preventDefault();
+              navigation.navigate("Profile", {uid: this.props.currentUser.uid});
+          }
+      })}
         options={{
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="account-circle" color={color} size={26} />
--- components/auth/firebaseConfig.js
+++ components/auth/firebaseConfig.js
@@ -1,20 +1,20 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
-import { getFirestore } from "firebase/firestore";
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAXTh32MG_KBkz0CJNJzJk2y748XFpQTGc",
-    authDomain: "sportsmate-21006.firebaseapp.com",
-    projectId: "sportsmate-21006",
-    storageBucket: "sportsmate-21006.appspot.com",
-    messagingSenderId: "411877771192",
-    appId: "1:411877771192:web:81b2c2e41a0bfaf4a9314d",
-    measurementId: "G-ED73N504HZ",
+  apiKey: "AIzaSyAXTh32MG_KBkz0CJNJzJk2y748XFpQTGc",
+  authDomain: "sportsmate-21006.firebaseapp.com",
+  projectId: "sportsmate-21006",
+  storageBucket: "sportsmate-21006.appspot.com",
+  messagingSenderId: "411877771192",
+  appId: "1:411877771192:web:81b2c2e41a0bfaf4a9314d",
+  measurementId: "G-ED73N504HZ",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-const firestore = getFirestore(app);
+const db = getFirestore(app);
 
-export { app, database };
+export { app, database, db };
